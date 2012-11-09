23b4351375a31cab8b4d7dd574c3226850fee692
Tn6o <tn6o.dev@gmail.com>
2012-11-09 16:47:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nogugle::Application.config.secret_token = 'f665734430c7d9135e289a80fb62716b945f915fa4f07c9bb6d958f9230899706f19927bf37164310d1cc6a4745540f526b186f5299d07b835cc11a111ad9ae5'
