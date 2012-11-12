b75a4c8807a75e1e611dd32c81d4b5ebe5eba220
Simon <smn.bonnard@gmail.com>
2012-11-12 11:31:50 +0100

    stop words list modified

--- lib/SearchingTools/Indexer/indexer.rb
+++ lib/SearchingTools/Indexer/indexer.rb
@@ -11,18 +11,18 @@ module SearchingTools
 
 	  		def tag_weights
 	  			tag_weights = {
-	  				:p => 1,
-	  				:h1 => 3,
-	  				:h2 => 1.5,
-	  				:h3 => 1.4,
-		            :h4 => 1.3,
-		            :h5 => 1.2,
-		            :h6 => 1.1,
-		            :strong => 2,
-		            :img => 1.1, # to change : img.alt
-		            :a => 1.1, # to change : a.title
-		            :header => 2, # to change : header.keyword, header.description, header.title
-		            :link => 4
+  	  				:p => 1,
+  	  				:h1 => 3,
+  	  				:h2 => 1.5,
+  	  				:h3 => 1.4,
+  		            :h4 => 1.3,
+  		            :h5 => 1.2,
+  		            :h6 => 1.1,
+  		            :strong => 2,
+  		            :img => 1.1, # to change : img.alt
+  		            :a => 1.1, # to change : a.title
+  		            :header => 2, # to change : header.keyword, header.description, header.title
+  		            :link => 4
 	  			}
 	  		end
 
@@ -31,8 +31,8 @@ module SearchingTools
 	  		end
 
 	  		def excluded_words
-	  			%w{le la les je tu il elle nous vous elles ils}
-	  		end
+          %w{le la les je tu il elle nous vous elles ils leur leurs là mes ma mon mais ou est donc or ni car ni si un une des}
+        end
 
 	  	end
 	  end
