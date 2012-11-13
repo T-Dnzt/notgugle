f1de1a187fb38d53b2ff4b33c9cba91cc379291f
Tn6o <tn6o.dev@gmail.com>
2012-11-13 14:38:46 +0100

    Searcher done.

--- lib/SearchingTools/Searcher/searcher.rb
+++ lib/SearchingTools/Searcher/searcher.rb
@@ -6,32 +6,46 @@ module SearchingTools
   			Rails.logger.info "Searching..."
   			searched_arr = searched_str.split(' ').each {|w| w.gsub!(/[^[:alpha:]]/, "")}
 
-  			result_pages = []
+  			result_pages = Hash.new(0)
 
         db = Mongo::Connection.new.db('notgugle-development')
         words_collection = db.collection("keywords")
   			searched_arr.each do |word|
-          Rails.logger.info "Looking for #{word}"
   			  matches = words_collection.find({'word' => /#{word}/i}).to_a
-          Rails.logger.info "Matches : #{matches.inspect}"
-          matches.each do |match|
-            
-            pages = match["pages"].sort! { |a,b| b["weight"] <=> a["weight"] }
 
-            pages.each {|page| result_pages << page }
-          end
+          pages = []
+          matches.each {|match| match["pages"].each { |p| pages << p } }
+          result_pages = sum_weight(get_result_pages(pages, "filename"))
   			end
 
-
-  			Rails.logger.debug "Results :"
-  			result_pages.each {|r| Rails.logger.debug "#{r}"}
+        result_pages.sort! {|a, b| b["weight"] <=> a["weight"] }
   			result_pages
   		end
 
+      def sum_weight(array)
+        array.each do |hash|
+          hash["weight"] = hash["weight"].inject{|sum,x| sum + x } if hash["weight"].class == Array
+        end
+        array
+      end
+
+      def get_result_pages(array, id)
+        array.group_by {|row| row[id] }.collect do |k,v|
+          keys = v.collect {|rec| rec.keys}.flatten.uniq
+          group = {}
+          keys.each do |key|
+            vals = v.collect { |rec| rec[key] }.uniq.compact
+            group[key] = vals.size > 1 ? vals : vals.first
+          end
+          group
+        end
+      end
 
   		def excluded_words
   			%w{le la les}
-  		end
+      end
+
+
   	  end
   	end
   end
